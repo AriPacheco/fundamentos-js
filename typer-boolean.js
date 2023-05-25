fd738479fbf3311754e580ed54d6858399a96129
Ariana Pacheco Pereira <arianapacheco_p@hotmail.com>
2023-05-24 21:36:06 -0300

    exemplos da aula 1

---
+++ typer-boolean.js
@@ -0,0 +1,18 @@
+//boolean
+// true -> verdadeiro
+//false -> falso
+//diferença de const e let
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivo = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+
+const  texto1 = "Ariana";
+const texto2 = "Elisangela";
+
+console.log(texto1 === texto2)
+
+
---
+++ typer-strings.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá, mundo!"; //String "" e ''
+const texto2 =  'Olá, munddo!';
+const senha = "senhaSuperSegura4566!";
+//caracteres como, por exemplo, do alfabeto; podemos guardar pontuação;
+// podemos guardar, basicamente, o que quisermos dentro dela
+const StringDeNumeors = "34656";
+
+//const citacao = 'O Leo disse "oi"'; //para fazer citações usar aspas
+//console.log(citacao)
+
+//template string ou template literal
+
+//concatenação (+) serve para somar texto
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+console.log(citacao + meuNome); 
+
